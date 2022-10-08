797181c7aba225960a2cc4c3215988bc5eb563a8
Mayday66 <115358608+Mayday66@users.noreply.github.com>
2022-10-08 23:45:53 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -13,8 +13,8 @@ const USER_CONFIG = {
   // 使用微信测试号：公众号APP_SECRET
   APP_SECRET: '02c0139b2a282ae361faefa23fe1ef2b',
 
-  PROVINCE: '福建',
-  CITY: '福州市',
+  PROVINCE: '广东',
+  CITY: '惠州',
 
   USERS: [
     {
