c0fd505387a0cb631800f8cce987002e5495aee6
Mayday66 <115358608+Mayday66@users.noreply.github.com>
2022-10-08 23:36:55 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oq9wm6tZovPb_BRZXNaDglbp8scw',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'OmXt82_FPXupeqXM5FJH50AgodZvGMJbNt7UK4QThr4',
+      useTemplateId: 'WDqG86jLaI7-pLOLHoHzL1W-pOi6VMBp3hHVBnun4xs',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
@@ -51,7 +51,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'OmXt82_FPXupeqXM5FJH50AgodZvGMJbNt7UK4QThr4',
+  CALLBACK_TEMPLATE_ID: 'WDqG86jLaI7-pLOLHoHzL1W-pOi6VMBp3hHVBnun4xs',
 
   CALLBACK_USERS: [
     {
